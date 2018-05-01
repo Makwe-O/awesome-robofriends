9e32a5634f85e1b0fcd935d203eae79a7e8be231
Mmakwe Onyeka <notmaks@yahoo.com>
2018-05-01 11:08:37 +0100

    Added monsters from api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card'
 import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+import { robots } from './robots'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<div>
+                  <Card id={robots[0].id} name={robots[0].name} username={robots[0].username} email={robots[0].email}/>
+                  <Card id={robots[1].id} name={robots[1].name} username={robots[1].username} email={robots[1].email}/>
+                  <Card id={robots[2].id} name={robots[2].name} username={robots[2].username} email={robots[2].email}/>
+                  <Card id={robots[3].id} name={robots[3].name} username={robots[3].username} email={robots[3].email}/>
+                </div>
+                , document.getElementById('root'));
+
 registerServiceWorker();
---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Mmakwe Onyeka Blessing',
+    username: 'notmaks',
+    email: 'notmaks@yahoo.com'
+  },
+  {
+    id: 2,
+    name: 'Festus Agbomma',
+    username: 'Yuma',
+    email: 'fustusyuma@yahoo.com'
+  },
+  {
+    id: 3,
+    name: 'Chigozie Henry',
+    username: 'weeder man',
+    email: 'henryokebugu@gmail.com'
+  },
+  {
+    id: 4,
+    name: 'Olushakin Best',
+    username: 'Shaki',
+    email: 'yemibest@yahoo.com'
+  },
+  {
+    id: 5,
+    name: 'Nzei Uche Jude',
+    username: 'Nzei money',
+    email: 'nzeijude@yahoo.com'
+  },
+  {
+    id: 6,
+    name: 'Suleiman Owoduni',
+    username: 'Sully',
+    email: 'suleimanowoduni@yahoo.com'
+  },
+  {
+    id: 7,
+    name: 'Omodemi yomi',
+    username: 'demigod',
+    email: 'yomiomodemi@gmail.com'
+  },
+  {
+    id: 1,
+    name: 'olamide jegede',
+    username: 'lami',
+    email: 'olamidejegede94@yahoo.com'
+  },
+]
